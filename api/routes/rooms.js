b6c9f3e5a87bd734f16feb3149316d83b18792ac
mutukuian <mutukui940@gmail.com>
2024-04-29 20:41:16 +0300

    add roomscontroller schema and route to finish the backend server

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,20 @@
-import express from "express"
+import express from "express";
+import { createError } from "../utils/error.js";
+import {createRoom, deleteRoom, getAllRooms, getRoom, updateRoom} from "../controller/room_controller.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router()
+
+
+//CREATE
+router.post("/:hotelId",verifyAdmin,createRoom);
+//UPDATE
+router.put("/:id",verifyAdmin,updateRoom);
+//DELETE
+router.delete("/:id/:hotelId",verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoom);
+//GETALL
+router.get("/",getAllRooms);
 
 export default router
